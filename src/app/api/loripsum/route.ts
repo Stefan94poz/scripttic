f64f445ea44398394e903f2ee929825f93869a02
stefaneze <stefan.s.webdev@gmail.com>
2024-03-07 23:33:03 +0100

    constant change

--- src/app/api/loripsum/route.ts
+++ src/app/api/loripsum/route.ts
@@ -1,9 +1,9 @@
-import { LOREM_IPSUM_FETCH_DATA_API } from "@/libs/constants";
+import { LOREM_IPSUM_FETCH_DATA } from "@/libs/constants";
 // Dev mode is always dynamic thats why we need to add this so it would work on production
 export const dynamic = "force-dynamic";
-export async function GET(request: Request) {
+export async function GET() {
   try {
-    const response = await fetch(LOREM_IPSUM_FETCH_DATA_API, {
+    const response = await fetch(LOREM_IPSUM_FETCH_DATA, {
       cache: "no-store",
     });
 
--- src/libs/constants.tsx
+++ src/libs/constants.tsx
@@ -1,4 +1,4 @@
-export const LOREM_IPSUM_FETCH_DATA_API = `https://loripsum.net/generate.php?p=${process.env.API_KEY}&l=medium&d=1&a=1&co=1&ul=1&ol=1&dl=1&bq=1&h=1`;
+export const LOREM_IPSUM_FETCH_DATA = `https://loripsum.net/generate.php?p=${process.env.API_KEY}&l=medium&d=1&a=1&co=1&ul=1&ol=1&dl=1&bq=1&h=1`;
 
 export const LOREM_IPSUM_API = `${process.env.API_URL}/api/loripsum`;
 export const pages = [
